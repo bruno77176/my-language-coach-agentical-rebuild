39c1dfb1861c40c53046378a366164cf28559eb9
Bruno Moise <bruno.a.moise@gmail.com>
2026-05-10 12:12:19 +0200

    fix(mobile): add react-native-worklets/plugin to babel config (required by reanimated 4 + @gorhom/bottom-sheet)

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -5,5 +5,9 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+    plugins: [
+      // MUST be last. Required by react-native-reanimated 4 / @gorhom/bottom-sheet.
+      "react-native-worklets/plugin",
+    ],
   };
 };
